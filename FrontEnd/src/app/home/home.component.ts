699fbb375b4e8893fd5e9090f78df10e69a77c36
KennethHann0324 <khannigan@student.neumont.edu>
2025-05-30 11:28:59 -0600

    Added styling for Front page, Sign up, and Log in

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import {AppCookieService} from '../cookie.service';
-import {CommonModule} from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   template: `
-    <h1>Welcome to Gambling99!</h1>
-    <p *ngIf="cookieValue == ''">Please Log in to continue</p>
-    <p *ngIf="cookieValue != ''">Please select a lobby to play</p>
+    <div class="container d-flex justify-content-center align-items-center ">
+      <div class="text-center">
+        <h1 class="">Welcome to Gambling99!</h1>
+        <p *ngIf="cookieValue == ''">Please Log in to continue</p>
+        <p *ngIf="cookieValue != ''">Please select a lobby to play</p>
+        <img ngSrc="./images/frontpage.png" height="599" width="918">
+      </div>
+    </div>
   `,
   styles: []
 }) //Potentially add a button on the home page to join a random game
